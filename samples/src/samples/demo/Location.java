0ed478cf9a5059a6d8c409a580f515650ddcb1f0
Zaki Armindo <zakiarmindo@gmail.com>
2021-03-18 19:06:01 +0800

    First commit for location.java

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,17 @@
+package samples.demo;
+
+public class Location {
+
+	private String locationName;
+	private int locationID;
+	
+	public String getLocationName(){
+		return locationName;
+	}
+	
+	public void setLocationName(String locationName) {
+		this.locationName= locationName;
+	}
+	
+	
+} 
