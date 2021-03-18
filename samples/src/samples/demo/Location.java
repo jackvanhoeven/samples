773d801a93bf416275e5d8da49015d8e70438c30
Zaki Armindo <zakiarmindo@gmail.com>
2021-03-18 19:12:11 +0800

    second commit for location.java

--- samples/src/samples/demo/Location.java
+++ samples/src/samples/demo/Location.java
@@ -4,6 +4,7 @@ public class Location {
 
 	private String locationName;
 	private int locationID;
+	private double area;
 	
 	public String getLocationName(){
 		return locationName;
@@ -13,5 +14,20 @@ public class Location {
 		this.locationName= locationName;
 	}
 	
+	public int getLocationID(){
+		return locationID;
+	}
+	
+	public void setLocationName(int locationID) {
+		this.locationID= locationID;
+	}
+	
+	public double getArea(){
+		return area;
+	}
+	
+	public void setArea(double area) {
+		this.area= area;
+	}
 	
 } 
